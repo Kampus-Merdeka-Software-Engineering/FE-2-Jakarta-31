b73d9ccb4c51c64cf9204bdb7fac6dbe3419085b
maisie yunita <maisieyunitaa@gmail.com>
2023-12-05 16:04:09 +0700

    update subscribe

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,39 +10,34 @@ document.querySelector(".close").onclick = function () {
   convas.classList.remove("active");
 };
 
+// BUTTON SUBSCRIBE
 function subscribe() {
-  // Mendapatkan nilai email dari input
   const email = document.getElementById("email").value;
   const messageElement = document.getElementById("subscription-message");
 
-  // Menampilkan pesan terima kasih jika email diisi
   if (email) {
     alert("Thank's for subscribing!🤩");
     closeSubscribeForm();
-    // Anda bisa menambahkan logika lain di sini, misalnya mengirim data ke server
   } else {
-    // Menampilkan pesan jika email kosong
-    alert("Please enter your email address first.");
+    alert("Please enter your email address first!");
   }
-
-  // Menutup form subscribe setelah melakukan subscribe (Anda bisa memodifikasi ini sesuai kebutuhan)
 }
 
 function closeSubscribeForm() {
   document.querySelector(".subscribe-form").classList.remove("active");
 }
 
-// BUTTON SUBSCRIBE NAVBAR
+// Button Subscribe Navbar
 document.querySelector("#open-subscribe-form").addEventListener("click", function () {
   document.querySelector(".subscribe-form").classList.add("active");
 });
 
-// BUTTON SUBSCRIBE MOBILE
+// Button Subscribe Mobile
 document.querySelector("#open-subscribe-form-main").addEventListener("click", function () {
   document.querySelector(".subscribe-form").classList.add("active");
 });
 
-// CLOSE BUTTON
+// Close Button
 document.querySelector(".subscribe-form .close-btn").addEventListener("click", function () {
   closeSubscribeForm();
 });
